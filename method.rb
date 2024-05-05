ac4edff4391f2d3c4f7d35f8e1bb35e2291d69bf
OEM Configuration (temporary user) <everton.alves.luz@hotmail.com>
2024-05-05 12:28:06 -0300

    return, métodos, select para consulta em ruby

---
+++ method.rb
@@ -0,0 +1,14 @@
+#def talk
+ #   puts 'Olá, como você está?'
+#end
+
+#talk
+
+def talk (first_name, last_name)
+    puts "Olá #{first_name} #{last_name}, como você está"
+end
+
+first_name = 'Everton'
+last_name = 'Luiz'
+
+talk(first_name, last_name)
